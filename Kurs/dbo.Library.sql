c825b9cc7ab91fbeecbeae74718c276acce5f802
WitcherFromRivia <48452428+WitcherFromRivia@users.noreply.github.com>
2019-05-23 23:31:15 +0300

    Add files via upload

---
+++ Kurs/dbo.Library.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Library] (
+    [Id]                   INT NOT NULL,
+    [Название книги]       NVARCHAR (50) NULL,
+    [Автор]                NVARCHAR (50) NULL,
+    [Год издания]          NVARCHAR (50) NULL,
+    [Издательство]         NVARCHAR (50) NULL,
+    [Цена в гривнах]       NVARCHAR (50) NULL,
+    [Состояние]            NVARCHAR (50) NULL,
+    [Дата выдачи читателю] NVARCHAR (50) NULL,
+
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
